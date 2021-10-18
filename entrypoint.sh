216f2d36c2d892f177c74f2c25ee1e4404a191b8
Edie Pasek <epasek@forevercompanies.com>
2021-10-18 14:21:02 -0500

    Move Producer to more stable solution

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-sudo mkdir ~/.aws/
-sudo chown 1000:1000 ~/.aws/
-
-aws configure set aws_access_key_id $ROUTE53_KEY
-aws configure set aws_secret_access_key $ROUTE53_SECRET
-sudo aws configure set aws_access_key_id $ROUTE53_KEY
-sudo aws configure set aws_secret_access_key $ROUTE53_SECRET
 
 sudo cron -f &
 sudo cat /hoster.sh.template | sed "s/NGINX/$NGINX/g" | sed "s/MAG_NAME/$MAG_NAME/g" > /tmp/hoster.sh
