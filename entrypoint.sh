2b768cc9d5a60f49549c4d084f2839a10c43a34a
Edie Pasek <epasek@forevercompanies.com>
2021-10-20 22:13:36 -0500

    Fix cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ sudo cron -f &
 sudo cat /hoster.sh.template | sed "s/NGINX/$NGINX/g" | sed "s/MAG_NAME/$MAG_NAME/g" > /tmp/hoster.sh
 sudo mv /tmp/hoster.sh /hoster.sh
 cat /hoster.sh
-echo "* * * * * sudo su -l root -c 'bash /hoster.sh' >> /var/www/magento/var/log/varnish.log" | crontab -
+echo "* * * * * sudo bash /hoster.sh >> /var/www/magento/var/log/varnish.log" | crontab -
